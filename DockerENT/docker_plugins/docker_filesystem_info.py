63eedd942c97f93806dd985cc782c41f8ee58d85
Rohit Sehgal <rohitsehgal1994@gmail.com>
2020-07-09 16:44:06 +0530

    Add extra check scan in filesystem info plugin

--- DockerENT/docker_plugins/docker_filesystem_info.py
+++ DockerENT/docker_plugins/docker_filesystem_info.py
@@ -1,4 +1,7 @@
 """Docker file-system-info scan plugin."""
+from DockerENT.utils import utils
+
+import docker
 import logging
 
 _log = logging.getLogger(__name__)
@@ -26,27 +29,65 @@ def scan(container, output_queue):
     """
     res = {}
 
-    _log.info('Staring {} Plugin ...'.format(_plugin_name_))
+    _log.info('Starting {} Plugin ...'.format(_plugin_name_))
+
+    api_client = docker.APIClient()
+    docker_inspect_output = api_client.inspect_container(container.short_id)
 
     driveinfo = {
         "MOUNT": {
+            "isDockerAttribute": False,
             "cmd": "mount",
             "msg": "Mount results",
             "results": []
         },
         "FSTAB": {
+            "isDockerAttribute": False,
             "cmd": "cat /etc/fstab",
             "msg": "fstab entries",
+            "results": []
+        },
+        "RW_VOLUMES": {
+            "isDockerAttribute": True,
+            "attribute_name": "VolumesRW",
+            "msg": "R/W volumes mounted in docker.",
+            "results": []
+
+        },
+        "DOCKER_MOUNTS": {
+            "isDockerAttribute": True,
+            "attribute_name": "Mounts",
+            "msg": "List mounted volumes in docker.",
             "results": []
         }
     }
 
-    for item in driveinfo:
-        cmd = driveinfo[item]['cmd']
-        output = container.exec_run(cmd).output.decode('utf-8')
-        result = output.split('\n')
-        driveinfo[item]['results'] = result
-        del driveinfo[item]['cmd']
+    for item in driveinfo.keys():
+        _log.info(item)
+        if driveinfo[item]['isDockerAttribute']:
+            option_result = utils.get_value_from_str_dotted_key(
+                docker_inspect_output,
+                driveinfo[item]['attribute_name']
+            )
+            del driveinfo[item]['attribute_name']
+
+            # Key not found
+            if option_result is None:
+                del driveinfo[item]['isDockerAttribute']
+                continue
+            elif isinstance(option_result, list):
+                driveinfo[item]['results'].extend(option_result)
+            else:
+                driveinfo[item]['results'].append(option_result)
+
+        else:
+            cmd = driveinfo[item]['cmd']
+            output = container.exec_run(cmd).output.decode('utf-8')
+            result = output.split('\n')
+            driveinfo[item]['results'] = result
+            del driveinfo[item]['cmd']
+
+        del driveinfo[item]['isDockerAttribute']
 
     result = driveinfo
 
