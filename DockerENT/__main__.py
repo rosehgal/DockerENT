4342406bf47f85d03914ba6030c2833e3eae201a
Rohit Sehgal <rohitsehgal1994@gmail.com>
2020-08-24 08:17:16 +0530

    Arg parsing handled by __main__

--- DockerENT/__main__.py
+++ DockerENT/__main__.py
@@ -6,10 +6,92 @@ from DockerENT import config_parser
 from DockerENT import controller
 
 import logging.config
+import argparse
 
 # Setup config for this package
 logger_config = config_parser.config['logger']
 logging.config.dictConfig(logger_config)
 
+# Read arguments for DockerENT application
+parser = argparse.ArgumentParser(
+    prog='Find the vulnerabilities hidden in your running container(s).'
+)
+docker_args_group = parser.add_argument_group()
+docker_args_group.add_argument(
+    '-d',
+    '--docker',
+    nargs='?',
+    dest='docker_container',
+    const='all',
+    help='Run scan against the running container.')
+docker_args_group.add_argument(
+    '-p',
+    '--plugins',
+    nargs='?',
+    dest='docker_plugins',
+    const='all',
+    help='Run scan with only specified plugins.')
+
+docker_nw_args_group = parser.add_argument_group()
+docker_nw_args_group.add_argument(
+    '-d-nw',
+    '--docker-network',
+    nargs='?',
+    dest='docker_network',
+    const='all',
+    help='Run scan against running docker-network.')
+docker_args_group.add_argument(
+    '-p-nw',
+    '--nw-plugins',
+    nargs='?',
+    dest='docker_nw_plugins',
+    const='all',
+    help='Run scan with only specified plugins.')
+
+parser.add_argument(
+    '-n',
+    '--process',
+    nargs='?',
+    dest='process_count',
+    default=2,
+    type=int,
+    help='Run scans in parallel.'
+)
+parser.add_argument(
+    '-a',
+    '--audit',
+    dest='audit',
+    action='store_true',
+    default=False
+)
+
+output_plugin = parser.add_argument_group()
+output_plugin.add_argument(
+    '-o',
+    '--output',
+    nargs='?',
+    dest='output',
+    default='file',
+    type=str,
+    help='Output plugin to write data to'
+)
+
+args = parser.parse_args()
+process_count = args.process_count
+output = args.output
+audit = args.audit
+
+docker_containers = args.docker_container
+docker_plugins = args.docker_plugins
+
+docker_nws = args.docker_network
+docker_nw_plugins = args.docker_nw_plugins
+
 # Start DockerENT
-controller.main()
+controller.main(docker_containers=docker_containers,
+                docker_plugins=docker_plugins,
+                docker_nws=docker_nws,
+                docker_nw_plugins=docker_nw_plugins,
+                process_count=process_count,
+                audit=audit,
+                output=output)
--- DockerENT/controller.py
+++ DockerENT/controller.py
@@ -12,85 +12,17 @@ import multiprocessing
 _log = logging.getLogger(__name__)
 
 
-def main():
+def main(docker_containers,
+         docker_plugins,
+         docker_nws,
+         docker_nw_plugins,
+         process_count,
+         audit,
+         output):
     """Start DockerENT application.
 
     :return: None
     """
-    parser = argparse.ArgumentParser(
-        prog='Find the vulnerabilities hidden in your running container(s).'
-    )
-    docker_args_group = parser.add_argument_group()
-    docker_args_group.add_argument(
-        '-d',
-        '--docker',
-        nargs='?',
-        dest='docker_container',
-        const='all',
-        help='Run scan against the running container.')
-    docker_args_group.add_argument(
-        '-p',
-        '--plugins',
-        nargs='?',
-        dest='docker_plugins',
-        const='all',
-        help='Run scan with only specified plugins.')
-
-    docker_nw_args_group = parser.add_argument_group()
-    docker_nw_args_group.add_argument(
-        '-d-nw',
-        '--docker-network',
-        nargs='?',
-        dest='docker_network',
-        const='all',
-        help='Run scan against running docker-network.')
-    docker_args_group.add_argument(
-        '-p-nw',
-        '--nw-plugins',
-        nargs='?',
-        dest='docker_nw_plugins',
-        const='all',
-        help='Run scan with only specified plugins.')
-
-    parser.add_argument(
-        '-n',
-        '--process',
-        nargs='?',
-        dest='process_count',
-        default=2,
-        type=int,
-        help='Run scans in parallel.'
-    )
-    parser.add_argument(
-        '-a',
-        '--audit',
-        dest='audit',
-        action='store_true',
-        default=False
-    )
-
-    output_plugin = parser.add_argument_group()
-    output_plugin.add_argument(
-        '-o',
-        '--output',
-        nargs='?',
-        dest='output',
-        default='file',
-        type=str,
-        help='Output plugin to write data to'
-    )
-
-    args = parser.parse_args()
-    process_count = args.process_count
-    output = args.output
-    audit = args.audit
-
-    docker_containers = args.docker_container
-    docker_plugins = args.docker_plugins
-
-    docker_nws = args.docker_network
-    docker_nw_plugins = args.docker_nw_plugins
-
     _log.info('Starting application ...')
 
     _log.info('Creating application pool space with count {}'
